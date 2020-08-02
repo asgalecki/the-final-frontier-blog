83857c22a8b8c427683e61934c2d902652d7e9fd
ag-vectivus <58975810+ag-vectivus@users.noreply.github.com>
2020-08-02 17:14:09 +0200

    Create basic content for footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import {Link} from 'gatsby';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <p className="footer__paragraph">Footer</p>
+        <section className="social-media">
+          <ul className="social-media__list">
+            <li className="social-media__link">
+              <Link className="social-media__link--link" to="/">Github</Link>
+            </li>
+            <li className="social-media__link">
+              <Link className="social-media__link--link" to="/">Facebook</Link>
+            </li>
+            <li className="social-media__link">
+              <Link className="social-media__link--link" to="/">Twitter</Link>
+            </li>
+            <li className="social-media__link">
+              <Link className="social-media__link--link" to="/">Instagram</Link>
+            </li>
+          </ul>
+        </section>
+
+        <section className="inner-links">
+        <ul className="inner-links__list">
+          <li className="inner-links__link">
+            <Link className="inner-links__link--link" to="/">Privacy</Link>
+          </li>
+          <li className="inner-links__link">
+            <Link className="inner-links__link--link" to="/">Contact</Link>
+          </li>
+        </ul>
+        </section>
+
+        <section className="newsletter">
+          <form className="newsletter__form">
+            <h5 className="newsletter__title">Subscribe to our Newsletter!</h5>
+            <div className="newsletter__input">
+              <label htmlFor="email">Email: </label>
+              <input type="email" id="email" />
+            </div>
+          </form>
+        </section>
       </div>
+      <div className="site-bottom">Created by ag-vectivus | 2020</div>
     </footer>
   )
 };
