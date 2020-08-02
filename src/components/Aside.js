d79a6f191152817988fd4e2366753485b63c9179
ag-vectivus <58975810+ag-vectivus@users.noreply.github.com>
2020-08-02 15:24:41 +0200

    Create basic content for main and aside.

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Aside = () => {
   return (
     <aside className="aside">
       <div className="aside__container">
-        <p className="aside__paragraph">Aside</p>
+        <section className="recent-posts">
+          <h3 className="recent-posts__title">Recent posts</h3>
+            <div className="recent-posts__post">
+              <h4 className="recent-posts__post--title">Title</h4>
+              <p className="recent-posts__date">2nd August 2020</p>
+            </div>
+            <div className="recent-posts__post">
+              <h4 className="recent-posts__post--title">Title</h4>
+              <p className="recent-posts__date">2nd August 2020</p>
+            </div>
+            <div className="recent-posts__post">
+              <h4 className="recent-posts__post--title">Title</h4>
+              <p className="recent-posts__date">2nd August 2020</p>
+            </div>
+        </section>
+        <section className="tags">
+          <h3 className="tags__title">Tags</h3>
+          <p className="tags__paragraph">
+            <Link className="tags__link" to="./">#Space</Link>, 
+            <Link className="tags__link" to="./">#Sun</Link>, 
+            <Link className="tags__link" to="./">#Moon</Link>
+          </p>
+        </section>
+        <section className="links">
+          <h3 className="links__title">Links</h3>
+          <ul className="links__list">
+            <li className="links__list--margin">
+              <Link className="links__link" to="./">nasa</Link>
+            </li>
+            <li className="links__list--margin">
+              <Link className="links__link" to="./">esa</Link>
+            </li>
+          </ul>
+        </section>
       </div>
     </aside>
   )
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Main = () => {
   return (
     <main className="main">
       <div className="main__container">
-        <p className="main__paragraph">Main</p>
+        <section className="post-card">
+          <img
+            className="post-card__image"
+            src="https://source.unsplash.com/collection/235/4928×3280"
+            alt="Random space photo from NASA"
+          />
+          <article className="post-card__details">
+            <h1 className="post-card__title">Title</h1>
+            <p className="post-card__date">2nd August 2020</p>
+            <p className="post-card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit distinctio quas minima dignissimos, placeat, pariatur repellendus sequi quo quam accusamus iure? Beatae laborum recusandae iusto maxime repellat omnis officiis accusamus...
+            <Link className="post-card__link" to="/">Read more</Link></p>
+          </article>
+        </section>
+        <section className="post-card">
+          <img
+            className="post-card__image"
+            src="https://source.unsplash.com/collection/235/4928×3280"
+            alt="Random space photo from NASA"
+          />
+          <article className="post-card__details">
+            <h1 className="post-card__title">Title</h1>
+            <p className="post-card__date">2nd August 2020</p>
+            <p className="post-card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit distinctio quas minima dignissimos, placeat, pariatur repellendus sequi quo quam accusamus iure? Beatae laborum recusandae iusto maxime repellat omnis officiis accusamus...
+            <Link className="post-card__link" to="/">Read more</Link></p>
+          </article>
+        </section>
+        <section className="post-card">
+          <img
+            className="post-card__image"
+            src="https://source.unsplash.com/collection/235/4928×3280"
+            alt="Random space photo from NASA"
+          />
+          <article className="post-card__details">
+            <h1 className="post-card__title">Title</h1>
+            <p className="post-card__date">2nd August 2020</p>
+            <p className="post-card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit distinctio quas minima dignissimos, placeat, pariatur repellendus sequi quo quam accusamus iure? Beatae laborum recusandae iusto maxime repellat omnis officiis accusamus...
+            <Link className="post-card__link" to="/">Read more</Link></p>
+          </article>
+        </section>
       </div>
     </main>
   )
