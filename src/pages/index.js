0d446238b19554279039541d15d68985910ae861
ag-vectivus <58975810+ag-vectivus@users.noreply.github.com>
2020-08-09 13:40:22 +0200

    Add post-card component, create about and contact pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Main from '../components/Main/Main';
 import Aside from '../components/Aside/Aside';
+import PostCard from '../components/Main/Post-card';
 
 export default function Home() {
   return (
     <Layout>
       <div className="layout__container">
-        <Main />
+        <Main>
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </Main>
         <Aside />
       </div>
     </Layout>
