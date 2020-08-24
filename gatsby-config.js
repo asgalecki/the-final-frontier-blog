f3b3e037876a620a718e0d8636fdf66ce324527d
ag-vectivus <58975810+ag-vectivus@users.noreply.github.com>
2020-08-24 11:56:18 +0200

    Add Netlify CMS. 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         },
       },
     },
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
