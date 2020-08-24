c4e7c4a9ef72f3ea28ecbb9eedc8e151001ae8ce
ag-vectivus <58975810+ag-vectivus@users.noreply.github.com>
2020-08-24 12:37:21 +0200

    bug fixed

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link, graphql } from 'gatsby';
 import kebabCase from 'lodash/kebabCase';
 
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import Main from '../components/Main/Main';
 import Aside from '../components/Aside/Aside';
 import SEO from "../components/Seo";
